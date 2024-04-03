1f4f850ce8836785b5899fc99a359b47f5896a3d
haru <godjaewook@naver.com>
2024-04-03 23:33:51 +0900

    준비

--- pages/01_DoucumentGPT.py
+++ pages/01_DoucumentGPT.py
@@ -36,3 +36,40 @@ if message:
 
     with st.sidebar:
         st.write(st.session_state)
+
+
+def save_api_key(api_key):
+    st.session_state["api_key"] = api_key
+    st.session_state["api_key_bool"] = True
+
+
+with st.sidebar:
+    file = st.file_uploader(
+        "Upload a .txt .pdf or .docx file",
+        type=["pdf", "txt", "docx"],
+    )
+
+with st.sidebar:
+    api_key = st.text_input(
+        "OPENAI_API_KEY를 넣어야 작동합니다.",
+        disabled=st.session_state["api_key"] is not None,
+    ).strip()
+
+    if api_key:
+        save_api_key(api_key)
+        st.write("API_KEY가 저장되었습니다.")
+
+    button = st.button("저장")
+
+    if button:
+        save_api_key(api_key)
+        if api_key == "":
+            st.write("API_KEY를 넣어주세요.")
+
+
+with st.sidebar:
+    st.write("Made by harry.")
+
+    st.write(
+        "https://github.com/lips85/normard-langchain/blob/main/pages/01_DoucumentGPT.py"
+    )
