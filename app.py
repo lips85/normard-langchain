4d85fe1a3364fa4daa404f1a6a286e4ace754484
haru <godjaewook@naver.com>
2024-04-04 22:46:07 +0900

    다시 시도...

--- app.py
+++ app.py
@@ -167,7 +167,7 @@ with st.sidebar:
         "https://github.com/lips85/normard-langchain/blob/main/pages/01_DoucumentGPT.py"
     )
 
-if st.session_state["api_key_bool"]:
+if st.session_state["api_key_bool"] == True:
     if file:
         retriever = embed_file(file)
         send_message("I'm ready! Ask away!", "ai", save=False)
