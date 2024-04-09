3afd741542e46abebd2d061bdf17d5b50d10b3bb
haru <godjaewook@naver.com>
2024-04-10 02:02:13 +0900

    warning 추가

--- pages/04_SiteGPT.py
+++ pages/04_SiteGPT.py
@@ -314,6 +314,9 @@ with st.sidebar:
 if not api_key:
     st.warning("Please provide an **:blue[OpenAI API Key]** on the sidebar.")
 
+if not openai_model == "선택해주세요":
+    st.warning("Please write down a **:blue[Sitemap URL]** on the sidebar.")
+
 if not url:
     st.warning("Please write down a **:blue[Sitemap URL]** on the sidebar.")
 
