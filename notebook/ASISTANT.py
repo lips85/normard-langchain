6d90b557ace13c8f76f9b24cee1f28e4c1d656d7
haru_company coder <godjaewook@naver.com>
2024-04-12 11:33:33 +0900

    assistant 실습

--- notebook/ASISTANT.py
+++
@@ -1,8 +0,0 @@
-from openai import OpenAI as client
-
-assistant = client.beta.assistants.create(
-    name="Math Tutor",
-    instructions="You are a personal math tutor. Answer questions briefly, in a sentence or less.",
-    model="gpt-4-turbo-preview",
-    language="kr",
-)
