cf53a2c8c650772a398813eabec732a250b4ee62
haru <godjaewook@naver.com>
2024-04-14 00:58:56 +0900

    과제 내용 삽입

--- pages/06_FinalAssitant.py
+++ pages/06_FinalAssitant.py
@@ -1,3 +1,17 @@
+
+# (EN)
+# Refactor the agent you made in the previous assignment into an OpenAI Assistant.
+# Give it a user interface with Streamlit that displays the conversation history.
+# Allow the user to use its own OpenAI API Key, load it from an st.input inside of st.sidebar
+# Using st.sidebar put a link to the Github repo with the code of your Streamlit app.
+
+# (KR)
+# 이전 과제에서 만든 에이전트를 OpenAI 어시스턴트로 리팩터링합니다.
+# 대화 기록을 표시하는 Streamlit 을 사용하여 유저 인터페이스를 제공하세요.
+# 유저가 자체 OpenAI API 키를 사용하도록 허용하고, st.sidebar 내부의 st.input에서 이를 로드합니다.
+# st.sidebar를 사용하여 Streamlit app 의 코드과 함께 깃허브 리포지토리에 링크를 넣습니다.
+
+
 import re
 import os
 import streamlit as st
